61794ca9146d3bff98eee96535f51de16d492a12
Alex Christian <aquispe.developer@gmail.com>
2020-07-27 12:15:46 -0500

    Agregado webhook para notificaciones

--- WhatsAppBot/controller/ChatApiController.js
+++ WhatsAppBot/controller/ChatApiController.js
@@ -1,27 +1,31 @@
 
-const ChatApiSdk = require('chat_api_sdk');
-const axios = require('axios')
+// const ChatApiSdk = require('chat_api_sdk');
+// const axios = require('axios')
+
+const tmpData = {
+  data: null
+}
 
 export default {
   load(app){
 
-    const defaultClient = ChatApiSdk.ApiClient.instance;
-    console.log("defaultClient", defaultClient)
+    // const defaultClient = ChatApiSdk.ApiClient.instance;
+    // console.log("defaultClient", defaultClient)
 
     // Configure API key authorization: instanceId
-    const instanceId = defaultClient.authentications['instanceId'];
-    instanceId.apiKey = "instance154076"
+    // const instanceId = defaultClient.authentications['instanceId'];
+    // instanceId.apiKey = "instance154076"
 
     // Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
     //instanceId.apiKeyPrefix['instanceId'] = "Token"
     // Configure API key authorization: token
-    const token = defaultClient.authentications['token'];
-    token.apiKey = "j2pm4q777tr4udtk"
+    // const token = defaultClient.authentications['token'];
+    // token.apiKey = "j2pm4q777tr4udtk"
 
     // Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
     //token.apiKeyPrefix['token'] = "Token"
 
-    const api = new ChatApiSdk.Class1InstanceApi
+    // const api = new ChatApiSdk.Class1InstanceApi
 
     // var callback = function(error, data, response) {
     //   if (error) {
@@ -32,31 +36,46 @@ export default {
     // };
     // api.expiry(callback);
 
+    // this.sendMessage(app, api)
+    // this.getCodeQR(app, api)
+
+
+
     this.statusConenction(app, api)
-    this.sendMessage(app, api)
-    this.getCodeQR(app, api)
+    this.hookChanges(app, api)
   },
   statusConenction(app, api){
     app.get('/connect', (req, res) => res.send('Connected'))
   },
-  sendMessage(app, api){
-    app.get('/chat', (req, res) => res.send('Test started'))
-  },
-  getCodeQR(app, api){
-    app.get('/qr/code', (req, res) => {
-      axios.get('https://api.chat-api.com/instance154076/qr_code?token=j2pm4q777tr4udtk').then((r) => {
-        console.log('getCodeQR', r.data)
-        res.send(r.data)
-      }).catch((e) => {
-        res.send(e)
-      })
+  hookChanges(app, api){
+    app.post('/mywebhook/chat_api', (req, res) => {
+        console.log('[LOG] hookChanges', req.data)
+        // tmpData.data = req.data
+        res.send(req.data)
+    }).catch((e)=>{
+      console.error("[ERROR] hookChanges", e)
     })
-
-    // api.
-    // const url = `https://api.chat-api.com/instance123/qr_code`
-    // axios.get()
-
-
-
   },
+
+
+
+
+
+
+
+
+  // sendMessage(app, api){
+  //   app.get('/chat', (req, res) => res.send('Test started'))
+  // },
+  // getCodeQR(app, api){
+  //   app.get('/qr/code', (req, res) => {
+  //     axios.get('https://api.chat-api.com/instance154076/qr_code?token=j2pm4q777tr4udtk').then((r) => {
+  //       console.log('getCodeQR', r.data)
+  //       res.send(r.data)
+  //     }).catch((e) => {
+  //       res.send(e)
+  //     })
+  //   })
+  // },
+
 }
