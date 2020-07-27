11f8422e6f2bd413a12a47a285f30fd873d2aa4e
Alex Christian <aquispe.developer@gmail.com>
2020-07-27 12:49:34 -0500

    Configurado para pruebas de webhook

--- WhatsAppBot/controller/ChatApiController.js
+++ WhatsAppBot/controller/ChatApiController.js
@@ -1,20 +1,26 @@
 
-const tmpData = {
-  data: null
-}
+let tmpData = null
 
 export default {
   load(app){
     this.statusConenction(app)
     this.hookChanges(app)
+    this.getData(app)
   },
   statusConenction(app){
     app.get('/connect', (req, res) => res.send('Connected'))
   },
   hookChanges(app){
     app.post('/mywebhook/chat_api', (req, res) => {
-        console.log('[LOG] hookChanges', JSON.stringify(req))
-        res.send(req)
+        console.log('[LOG] hookChanges', req.body)
+        tmpData = req.body
+        res.send(req.body)
+    })
+  },
+  getData(app){
+    app.get('/mywebhook/chat_api/data', (req, res) => {
+      // console.log('[LOG] hookChanges', req.body)
+      res.send(tmpData)
     })
   },
 
