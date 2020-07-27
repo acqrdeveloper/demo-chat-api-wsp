bb712f34badf9b0eea8525250e499f7fd526c2bd
Alex Christian <aquispe.developer@gmail.com>
2020-07-27 12:37:01 -0500

    Eliminado comentarios

--- WhatsAppBot/controller/ChatApiController.js
+++ WhatsAppBot/controller/ChatApiController.js
@@ -1,46 +1,10 @@
 
-// const ChatApiSdk = require('chat_api_sdk');
-// const axios = require('axios')
-
 const tmpData = {
   data: null
 }
 
 export default {
   load(app){
-
-    // const defaultClient = ChatApiSdk.ApiClient.instance;
-    // console.log("defaultClient", defaultClient)
-
-    // Configure API key authorization: instanceId
-    // const instanceId = defaultClient.authentications['instanceId'];
-    // instanceId.apiKey = "instance154076"
-
-    // Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
-    //instanceId.apiKeyPrefix['instanceId'] = "Token"
-    // Configure API key authorization: token
-    // const token = defaultClient.authentications['token'];
-    // token.apiKey = "j2pm4q777tr4udtk"
-
-    // Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
-    //token.apiKeyPrefix['token'] = "Token"
-
-    // const api = new ChatApiSdk.Class1InstanceApi
-
-    // var callback = function(error, data, response) {
-    //   if (error) {
-    //     console.error(error);
-    //   } else {
-    //     console.log('API called successfully. Returned data: ' + data);
-    //   }
-    // };
-    // api.expiry(callback);
-
-    // this.sendMessage(app, api)
-    // this.getCodeQR(app, api)
-
-
-
     this.statusConenction(app)
     this.hookChanges(app)
   },
@@ -50,30 +14,8 @@ export default {
   hookChanges(app){
     app.post('/mywebhook/chat_api', (req, res) => {
         console.log('[LOG] hookChanges', JSON.stringify(req))
-        // tmpData.data = req.data
         res.send(req)
     })
   },
-
-
-
-
-
-
-
-
-  // sendMessage(app, api){
-  //   app.get('/chat', (req, res) => res.send('Test started'))
-  // },
-  // getCodeQR(app, api){
-  //   app.get('/qr/code', (req, res) => {
-  //     axios.get('https://api.chat-api.com/instance154076/qr_code?token=j2pm4q777tr4udtk').then((r) => {
-  //       console.log('getCodeQR', r.data)
-  //       res.send(r.data)
-  //     }).catch((e) => {
-  //       res.send(e)
-  //     })
-  //   })
-  // },
 
 }
--- bin/server.js
+++ bin/server.js
@@ -1,6 +1,6 @@
 // requires
 const dotenv = require('dotenv')
-const cors = require('cors')
+// const cors = require('cors')
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
@@ -31,21 +31,21 @@ app.use(express.static('public'))
 
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
-app.use((req, res, next) => {
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', `${process.env.MYAPIURL}:${process.env.MYPORT}`)
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With')
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  // Pass to next layer of middleware
-  next()
-})
+// app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(cors())
+// app.use((req, res, next) => {
+//   // Website you wish to allow to connect
+//   res.setHeader('Access-Control-Allow-Origin', `${process.env.MYAPIURL}:${process.env.MYPORT}`)
+//   // Request methods you wish to allow
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+//   // Request headers you wish to allow
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With')
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader('Access-Control-Allow-Credentials', true)
+//   // Pass to next layer of middleware
+//   next()
+// })
 
 ChatApiController.load(app)
 
